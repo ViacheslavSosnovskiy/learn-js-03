a52e0d54278526d6dccdf0fbc7b5882ec3c9fdb0
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-05-25 21:47:47 +0300

    task-cart

---
+++ js/cart.js
@@ -0,0 +1,69 @@
+/*
+ * Работем с коллекцией товаров в корзине:
+ * - getItems()
+ * - add(product)
+ * - remove(productName)
+ * - clear()
+ * - countTotalPrice()
+ * - increaseQuantity(productName)
+ * - decreaseQuantity(productName)
+ *
+ * { name: '🍎', price: 50 }
+ * { name: '🍇', price: 70 }
+ * { name: '🍋', price: 60 }
+ * { name: '🍓', price: 110 }
+ */
+
+const cart = {
+  items: [],
+  getItems() {
+    return this.items;
+  },
+  add(product) {
+    this.items.push(product);
+  },
+  remove(productName) {
+    const { items } = this;
+
+    for (let i = 0; i < items.length; i += 1) {
+      const { name } = items[i];
+
+      if (productName === name) {
+        console.log("нашли такой продукт", productName);
+        console.log(i);
+
+        items.splice(i, 1);
+      }
+    }
+  },
+  clear() {
+    this.items = [];
+  },
+  countTotalPrice() {},
+  increaseQuantity(productName) {},
+  decreaseQuantity(productName) {},
+};
+
+console.log(cart.getItems());
+
+cart.add({ name: "🍎", price: 50 });
+cart.add({ name: "🍇", price: 60 });
+cart.add({ name: "🍋", price: 60 });
+cart.add({ name: "🍓", price: 110 });
+
+console.table(cart.getItems());
+
+cart.remove("🍎");
+console.table(cart.getItems());
+
+cart.clear();
+console.log(cart.getItems());
+
+// cart.increaseQuantity('🍎');
+// console.table(cart.getItems());
+
+// cart.decreaseQuantity('🍋');
+// cart.decreaseQuantity('🍋');
+// console.table(cart.getItems());
+
+console.log("Total: ", cart.countTotalPrice());
