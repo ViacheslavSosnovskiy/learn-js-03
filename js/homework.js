638b622aab5e2b6182bf35ecd6f64d34c7c9e525
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-05-29 22:23:06 +0300

    29 tasks done

--- js/homework.js
+++ js/homework.js
@@ -290,3 +290,243 @@ function getProductPrice(productName) {
   }
   return null;
 }
+
+// задача 19
+
+// Напиши функцию getAllPropValues(propName) которая принимает один параметр
+// propName - имя (ключ) свойства. Функция должна вернуть массив всех значений
+// свойства с таким именем из каждого объекта в массиве products.
+// Если в объектах нет свойства с таким именем, функция должна вернуть пустой массив.
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function getAllPropValues(propName) {
+  const emptyArray = [];
+
+  for (const product of products) {
+    if ("name" === propName) {
+      emptyArray.push(product.name);
+    } else if ("price" === propName) {
+      emptyArray.push(product.price);
+    } else if ("quantity" === propName) {
+      emptyArray.push(product.quantity);
+    }
+  }
+  return emptyArray;
+}
+
+// задача 20
+
+// Напиши функцию calculateTotalPrice(productName) которая принимает один
+// параметр productName - название товара.
+// Функция должна вернуть общую стоимость (цена * количество)
+// товара с таким именем из массива products.
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  let total = 0;
+
+  for (const product of products) {
+    if (product.name === productName) {
+      total = product.price * product.quantity;
+    }
+  }
+  return total;
+}
+
+// задача 21
+
+// Пришел трёхдневный прогноз максимальных температур и мы считаем среднюю
+// температуру за три дня (meanTemperature). Замени объявления переменных yesterday,
+// today и tomorrow одной операцией деструктуризации свойств объекта highTemperatures.
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+
+const { yesterday, today, tomorrow } = highTemperatures;
+
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// задача 22
+
+// В прогнозе максимальных температур также может быть необязательное свойство
+// icon - иконка погоды. Замени объявления переменных yesterday, today, tomorrow
+// и icon одной операцией деструктуризации свойств объекта highTemperatures.
+// Задай значение по умолчанию для
+// icon - строку "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+
+const {
+  icon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+  yesterday,
+  today,
+  tomorrow,
+} = highTemperatures;
+
+const meanTemperature = (yesterday + today + tomorrow) / 3;
+
+// задача 23
+
+// Замени объявления переменных highYesterday, highToday, highTomorrow
+// и highIcon одной операцией деструктуризации свойств объекта highTemperatures.
+// Задай значение по умолчанию для highIcon -
+// строку "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+
+const highTemperatures = {
+  yesterday: 28,
+  today: 26,
+  tomorrow: 33,
+};
+
+const {
+  yesterday: highYesterday,
+  today: highToday,
+  tomorrow: highTomorrow,
+  icon: highIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+} = highTemperatures;
+
+const meanTemperature = (highYesterday + highToday + highTomorrow) / 3;
+
+// задача 24
+
+// Выполни рефакторинг цикла for...of так, чтобы в нём использовалась деструктуризация объекта..
+
+const colors = [
+  { hex: "#f44336", rgb: "244,67,54" },
+  { hex: "#2196f3", rgb: "33,150,243" },
+  { hex: "#4caf50", rgb: "76,175,80" },
+  { hex: "#ffeb3b", rgb: "255,235,59" },
+];
+
+const hexColors = [];
+const rgbColors = [];
+
+for (const { hex, rgb } of colors) {
+  hexColors.push(hex);
+  rgbColors.push(rgb);
+}
+
+// задача 25
+
+// Мы получили прогноз погоды на два дня, с минимальными и максимальными температурами,
+// а также необязательными иконками. Замени объявления всех переменных одной операцией
+// деструктуризации свойств объекта forecast. Задай значение по умолчанию для иконок, переменных
+// todayIcon и tomorrowIcon - строку "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg".
+
+const forecast = {
+  today: {
+    low: 28,
+    high: 32,
+    icon: "https://www.flaticon.com/svg/static/icons/svg/861/861059.svg",
+  },
+  tomorrow: {
+    low: 27,
+    high: 31,
+  },
+};
+const {
+  today: {
+    low: lowToday,
+    high: highToday,
+    icon: todayIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+  },
+  tomorrow: {
+    low: lowTomorrow,
+    high: highTomorrow,
+    icon: tomorrowIcon = "https://www.flaticon.com/svg/static/icons/svg/2204/2204346.svg",
+  },
+} = forecast;
+
+// задача 26
+
+// Функция calculateMeanTemperature(forecast) принимает один параметр forecast
+// - объект температур на два дня следующего формата.
+//       today: { low: 10, high: 20 },
+//       tomorrow: { low: 20, high: 30 }
+// Замени объявления переменных todayLow, todayHigh,
+// tomorrowLow и tomorrowHigh одной операцией деструктуризации свойств объекта forecast.
+
+function calculateMeanTemperature(forecast) {
+  const {
+    today: { low: todayLow, high: todayHigh },
+    tomorrow: { low: tomorrowLow, high: tomorrowHigh },
+  } = forecast;
+
+  return (todayLow + todayHigh + tomorrowLow + tomorrowHigh) / 4;
+}
+
+// задача 27
+
+// В переменной scores хранится массив результатов тестирования.
+// Используя распыление и методы Math.max() и Math.min() дополни код так,
+// чтобы в переменной bestScore был самый высокий балл, а в worstScore самый низкий.
+
+const scores = [89, 64, 42, 17, 93, 51, 26];
+
+const bestScore = Math.max(...scores);
+const worstScore = Math.min(...scores);
+
+// задача 28
+
+// В переменных firstGroupScores, secondGroupScores и thirdGroupScores
+// хранятся результаты тестирования отдельных групп. Используя распыление дополни код так, чтобы:
+
+// В переменной allScores хранился массив всех результатов от первой до третьей группы.
+// В переменной bestScore был самый высокий общий балл.
+// В переменной worstScore был самый низкий общий балл.
+
+const firstGroupScores = [64, 42, 93];
+const secondGroupScores = [89, 14, 51, 26];
+const thirdGroupScores = [29, 47, 18, 97, 81];
+
+const allScores = [
+  ...firstGroupScores,
+  ...secondGroupScores,
+  ...thirdGroupScores,
+];
+const bestScore = Math.max(...allScores);
+const worstScore = Math.min(...allScores);
+
+// задача 29
+
+// В конструкторе можно создавать новые тесты, для которых есть настройки по умолчанию
+// которые хранятся в переменной defaultSettings. Во время создания теста, все или часть настроек
+// можно переопределить,
+// они хранятся в переменной overrideSettings.
+
+// Для того чтобы получить финальные настройки теста, необходимо взять настройки по
+// умолчанию и поверх них применить переопределённые настройки. Дополни код так, чтобы
+// в переменной finalSettings получился объект финальных настроек теста.
+const defaultSettings = {
+  theme: "light",
+  public: true,
+  withPassword: false,
+  minNumberOfQuestions: 10,
+  timePerQuestion: 60,
+};
+const overrideSettings = {
+  public: false,
+  withPassword: true,
+  timePerQuestion: 30,
+};
+
+const finalSettings = { ...defaultSettings, ...overrideSettings };
