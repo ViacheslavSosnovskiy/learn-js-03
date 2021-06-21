0aa14e1b067bc71c52a184f480fa76803d1cc2e5
ViacheslavSosnovskiy <saintstill333@gmail.com>
2021-06-21 22:33:28 +0300

    task object

--- js/object.js
+++ js/object.js
@@ -16,32 +16,50 @@
 //   email,
 // };
 
+// const playlist = {
+//   name: "Слава",
+//   tracks: ["мой плейлист"],
+//   rating: 5,
+//   changeName(newName) {
+//     console.log("this внутри changeName:", this);
+
+//     this.name = newName;
+//   },
+//   addTrack(track) {
+//     this.tracks.push(track);
+//   },
+//   updateRating(newRating) {
+//     this.rating = newRating;
+//   },
+//   getTrackCount() {
+//     return this.tracks.length;
+//   },
+// };
+
+// console.log(playlist.getTrackCount());
+
+// playlist.changeName("новое имя");
+
+// playlist.addTrack("новый трек");
+
+// playlist.updateRating(5);
+
+// console.log(playlist);
+
 const playlist = {
   name: "Слава",
-  tracks: ["мой плейлист"],
+  tracks: ["мой плейлист", "альбом 2005"],
   rating: 5,
-  changeName(newName) {
-    console.log("this внутри changeName:", this);
-
-    this.name = newName;
-  },
   addTrack(track) {
     this.tracks.push(track);
-  },
-  updateRating(newRating) {
-    this.rating = newRating;
   },
   getTrackCount() {
     return this.tracks.length;
   },
 };
 
+playlist.addTrack("НОВЫЙ АЛЬБОМ 2021");
 console.log(playlist.getTrackCount());
 
-playlist.changeName("новое имя");
-
-playlist.addTrack("новый трек");
-
-playlist.updateRating(5);
-
-console.log(playlist);
+playlist.addTrack("НОВЫЙ АЛЬБОМ 2022");
+console.log(playlist.getTrackCount());
